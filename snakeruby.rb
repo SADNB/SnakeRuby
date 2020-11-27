e53d8fa9e8a505f2dc4c122ecc9ae4d26f7faef7
SADNB <71390288+SADNB@users.noreply.github.com>
2020-11-27 19:17:54 +0100

    Add files via upload

--- snakeruby.rb
+++ snakeruby.rb
@@ -3,15 +3,17 @@ require 'ruby2d'
 set background: 'navy'
 set fps_cap: 20
 
-SQUARE_SIZE=20;
+SQUARE_SIZE = 20
 GRID_WIDTH = Window.width / SQUARE_SIZE
 GRID_HEIGHT = Window.height / SQUARE_SIZE
+
 class Snake
   attr_writer :direction
 
   def initialize
     @positions = [[2, 0], [2, 1], [2, 2], [2 ,3]]
     @direction = 'down'
+    @growing = false
   end
 
   def draw
@@ -20,17 +22,25 @@ class Snake
     end
   end
 
+  def grow
+    @growing = true
+  end
+
   def move
-    @positions.shift
+    if !@growing
+      @positions.shift
+    end
+
+    @positions.push(next_position)
+    @growing = false
+  end
+
+  def can_change_direction_to?(new_direction)
     case @direction
-    when 'down'
-      @positions.push([head[0], head[1] + 1])
-    when 'up'
-      @positions.push([head[0], head[1] - 1])
-    when 'left'
-      @positions.push([head[0] - 1, head[1]])
-    when 'right'
-      @positions.push([head[0] + 1, head[1]])
+    when 'up' then new_direction != 'down'
+    when 'down' then new_direction != 'up'
+    when 'left' then new_direction != 'right'
+    when 'right' then new_direction != 'left'
     end
   end
 
@@ -42,21 +52,93 @@ class Snake
     head[1]
   end
 
+  def next_position
+    if @direction == 'down'
+      new_coords(head[0], head[1] + 1)
+    elsif @direction == 'up'
+      new_coords(head[0], head[1] - 1)
+    elsif @direction == 'left'
+      new_coords(head[0] - 1, head[1])
+    elsif @direction == 'right'
+      new_coords(head[0] + 1, head[1])
+    end
+  end
+
+  def hit_itself?
+    @positions.uniq.length != @positions.length
+  end
+
+  private
+
+  def new_coords(x, y)
+    [x % GRID_WIDTH, y % GRID_HEIGHT]
+  end
+
   def head
     @positions.last
   end
 end
-snake=Snake.new
+
+class Game
+  def initialize
+    @ball_x = 10
+    @ball_y = 10
+    @score = 0
+    @finished = false
+  end
+
+  def draw
+    Square.new(x: @ball_x * SQUARE_SIZE, y: @ball_y * SQUARE_SIZE, size: SQUARE_SIZE, color: 'yellow')
+  end
+
+  def snake_hit_ball?(x, y)
+    @ball_x == x && @ball_y == y
+  end
+
+  def record_hit
+      @ball_x = rand(Window.width / SQUARE_SIZE)
+      @ball_y = rand(Window.height / SQUARE_SIZE)
+  end
+
+  def finish
+    @finished = true
+  end
+
+  def finished?
+    @finished
+  end
+
+end
+
+snake = Snake.new
+game = Game.new
 
 update do
   clear
-  snake.move
+
+  unless game.finished?
+    snake.move
+  end
+
   snake.draw
+  game.draw
+
+  if game.snake_hit_ball?(snake.x, snake.y)
+    game.record_hit
+    snake.grow
+  end
 end
 
 on :key_down do |event|
   if ['up', 'down', 'left', 'right'].include?(event.key)
-    snake.direction=event.key
+    if snake.can_change_direction_to?(event.key)
+      snake.direction = event.key
+    end
+  end
+  if game.finished? && event.key == 'r'
+    snake = Snake.new
+    game = Game.new
   end
 end
+
 show
